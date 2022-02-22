6666feb94daff3d173f875e5083a4171cc831e43
Limitiz <ll55kk678@gmail.com>
2022-02-22 17:01:44 +0900

    dfs 사용

--- BOJ/src/main/java/graph/BOJ_9205.java
+++ BOJ/src/main/java/graph/BOJ_9205.java
@@ -1,64 +1,69 @@
-package graph;
+ package graph;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+ import java.io.BufferedReader;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.util.Arrays;
+ import java.util.LinkedList;
+ import java.util.StringTokenizer;
 
- class Point{
-     int x,y;
-     Point(int x, int y){
-         this.x = x;
-         this.y = y;
-     }
- }
  public class BOJ_9205 {
-     public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         int t = Integer.parseInt(br.readLine());
-         while(t-->0){
-             //입력받기 (Point 클래스 사용)
-             int n = Integer.parseInt(br.readLine());
 
-             StringTokenizer st = new StringTokenizer(br.readLine());
-             Point cur = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+     static int n;
+     static boolean[] chk;
+     static LinkedList<int[]>[] graph;
 
-             Point[] mart = new Point[n];
-             for(int i=0; i<n; i++){
-                 st = new StringTokenizer(br.readLine());
-                 mart[i] =new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-             }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            //입력받기 (배열사용)
+            n = Integer.parseInt(br.readLine())+2;
+            //graph 만들기 (1000m 이하만 연결)
+            graph = new LinkedList[n];
+            chk = new boolean[n];
+            Arrays.fill(chk, false); //초기화
 
-             st = new StringTokenizer(br.readLine());
-             Point goal = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            int[][] pos = new int[n][3]; //num, x, y
+            for(int i=0; i<n; i++){
+                st = new StringTokenizer(br.readLine());
+                pos[i][0] = i;
+                pos[i][1] = Integer.parseInt(st.nextToken());
+                pos[i][2] = Integer.parseInt(st.nextToken());
+                graph[i] = new LinkedList<>(); //그래프 초기화
+            }
 
-             //맥주가 충분하면 그냥 가기
-             //맥주가 불충분하면 편의점 들리기
-             int last = -1;
-             while(true) {
-                 if (Math.abs(goal.x - cur.x) + Math.abs(goal.y - cur.y) <= 1000) { //50m당 맥주1병이니까
-                     System.out.println("happy");
-                     System.exit(0);
-                 }
-                 else {
-                     int i;
-                     for (i = n - 1; i > last; i--) {
-                         int distance = Math.abs(mart[i].x - cur.x) + Math.abs(mart[i].y-cur.y);
-                         if(distance <= 1000){
-                             cur.x = mart[i].x;
-                             cur.y = mart[i].y;
-                             break;
-                         }
-                     }
+            //graph 채우기
+            for(int i=0; i<n; i++){
+                for(int[] p : pos){
+                    if(Math.abs(p[1]-pos[i][1])+Math.abs(p[2]-pos[i][2]) <= 1000)
+                        graph[i].add(p);
+                }
+            }
 
-                     if(i == last) {
-                         System.out.println("sad");
-                         System.exit(0);
-                     } else last = i;
+            /** //그래프 확인코드
+            for(LinkedList<int[]> i : graph){
+                for(int[] j : i)
+                    System.out.print(j[0]+"("+j[1]+","+j[2]+")  ");
+                System.out.println();
+            }**/
 
-                 }
-             }
-         }
-     }
- }
+            //dfs
+            dfs(0);
+            if(chk[n-1]) System.out.println("happy");
+            else System.out.println("sad");
+        }
+    }
 
+    public static void dfs(int v){
+        if(v == n-1) {
+            chk[n-1] = true;
+            return;
+        }
+        if(!chk[v]){
+            chk[v] = true;
+            for(int[] i : graph[v]) dfs(i[0]);
+        }
+    }
+}
