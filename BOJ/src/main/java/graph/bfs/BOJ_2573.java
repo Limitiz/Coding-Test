d8d2a650d0777d9243f3039a71be9c0722429a0d
Limitiz <ll55kk678@gmail.com>
2022-02-24 17:46:16 +0900

    fix : 현황 업데이트 다 안했는데 빙산 개수 세버림

--- BOJ/src/main/java/graph/bfs/BOJ_2573.java
+++ BOJ/src/main/java/graph/bfs/BOJ_2573.java
@@ -3,6 +3,7 @@ package graph.bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -17,6 +18,11 @@ class Point{
     }
 }
 public class BOJ_2573 {
+
+
+    static boolean chk[][];
+    static int ice[][];
+
     public static void main(String[] args) throws IOException{
         //입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,9 +30,11 @@ public class BOJ_2573 {
         int n = Integer.parseInt(st.nextToken()); //행 y
         int m = Integer.parseInt(st.nextToken()); //열 x
 
-        int ice[][] = new int[n][m]; // 빙산 현황
-        Queue<Point> q = new LinkedList<>();
-        Queue<Point> tmp = new LinkedList<>();
+        ice = new int[n][m]; // 빙산 현황
+        chk = new boolean[n][m];
+        Queue<Point> q = new LinkedList<>(); //빙산 녹이기 할때 사용
+        Queue<Point> tmp = new LinkedList<>(); //빙산 현황 업데이트 할때 사용
+        Queue<Point> tmp2 = new LinkedList<>(); //빙산 개수 셀때 사용
         int time = 0, count; //시간, 빙산 개수
 
 
@@ -54,33 +62,53 @@ public class BOJ_2573 {
             }
             time++;
 
-            // /**
-            //확인용 코드
-            for(int[] i : ice){
-                for(int j : i)
-                    System.out.print(j+" ");
-                System.out.println();
-            }
-            System.out.println();
-             // **/
+            //checkStatus(ice); //확인용 코드
 
-            //빙산 개수 세기
-            count = 2;
+            //빙산 현황 업데이트
             while(!tmp.isEmpty()) {
                 Point cur = tmp.poll();
-                if(ice[cur.x-1][cur.y] == 0 && ice[cur.x+1][cur.y] == 0
-                        && ice[cur.x][cur.y-1] == 0 && ice[cur.x][cur.y+1] == 0)
-                    count--;
-
-                if(count == 0) { //빙산 2개 이상
-                    System.out.println(time);
-                    System.exit(0);
-                }
-
                 ice[cur.x][cur.y] = cur.num; //빙산현황 업데이트
-                q.add(cur);
+                if(cur.num != 0) {
+                    q.add(cur); tmp2.add(cur);
+                }
+            }
+
+            //빙산 개수 세기
+            //초기화
+            for(int i=0; i<n; i++)
+                Arrays.fill(chk[i], false);
+            count = 0;
+
+            while(!tmp2.isEmpty()){
+                Point cur = tmp2.poll();
+                if(chk[cur.x][cur.y]) continue;
+
+                search(cur.x, cur.y);
+                count++;
+            }
+            //System.out.println("count = "+count); //확인용 코드
+            if(count >= 2){
+                System.out.println(time);
+                System.exit(0);
             }
         }
         System.out.println(0);
+    }
+
+    public static void search(int x, int y){
+        chk[x][y] = true;
+        if(!chk[x-1][y] && ice[x-1][y] != 0) search(x-1, y);
+        if(!chk[x+1][y] && ice[x+1][y] != 0) search(x+1, y);
+        if(!chk[x][y-1] && ice[x][y-1] != 0) search(x, y-1);
+        if(!chk[x][y+1] && ice[x][y+1] != 0) search(x, y+1);
+    }
+
+    //확인용 코드
+    public static void checkStatus(int[][] ice){
+        for(int[] i : ice){
+            for(int j : i)
+                System.out.print(j+" ");
+            System.out.println();
+        }
     }
 }
