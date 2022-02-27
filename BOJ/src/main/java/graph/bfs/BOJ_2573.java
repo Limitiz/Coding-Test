1345ba9ce790368de0caa453cab46a9b8fcf55f5
Limitiz <ll55kk678@gmail.com>
2022-02-27 14:57:04 +0900

    refactoring

--- BOJ/src/main/java/graph/bfs/BOJ_2573.java
+++ BOJ/src/main/java/graph/bfs/BOJ_2573.java
@@ -3,10 +3,7 @@ package graph.bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Point{
     int num, x, y;
@@ -20,8 +17,8 @@ class Point{
 public class BOJ_2573 {
 
 
-    static boolean chk[][];
-    static int ice[][];
+    static boolean[][] chk;
+    static int[][] ice;
 
     public static void main(String[] args) throws IOException{
         //입력
