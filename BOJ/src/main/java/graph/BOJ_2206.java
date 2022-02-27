527543f36c16ecd8c3c515adc2a785a3d6bacd2d
Limitiz <ll55kk678@gmail.com>
2022-02-27 16:30:28 +0900

    fix : chk방법 바꿈 + 최단거리는 bfs

--- BOJ/src/main/java/graph/BOJ_2206.java
+++ BOJ/src/main/java/graph/BOJ_2206.java
@@ -4,14 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
+
+class Point{
+    int x,y,dist;
+    boolean hit;
+
+    Point(int x, int y, int dist, boolean hit){
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+        this.hit = hit;
+    }
+}
 
 public class BOJ_2206 {
 
     static int n, m;
     static int map[][];
+    static boolean[][][] chk;
     static int direc[][] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-    static int shortest = 1000000;
 
     public static void main(String[] args) throws IOException {
         //입력
@@ -20,65 +34,46 @@ public class BOJ_2206 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        boolean[][] chk = new boolean[n][m];
+        chk = new boolean[n][m][2]; //0 벽 안부시고 탐색, 1 벽 부시고 탐색
         map = new int[n][m];
+        
         for(int i=0; i<n; i++)
             map[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
 
-        dfs(0,0,1, chk, 1);
-
-        if(shortest < 1000000) System.out.println(shortest);
-        else System.out.println(-1);
+        System.out.println(bfs(0,0, 1)); //최단거리 구하는거니까 bfs 적합
     }
 
-    public static void dfs(int x, int y, int hit, boolean[][] chk, int count){
-        if(chk[x][y]) {
-            System.out.println("못했음");
-            return;
-        }
+    public static int bfs(int x, int y, int count) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y, count, false));
 
-        chk[x][y] = true;
-        System.out.println(y+","+x+"를 지난다. 현재 거리 "+count); //확인용 코드
+        while(!q.isEmpty()){
+            Point cur = q.poll();
 
-        if(x == n-1 && y == m-1){ //도착했을때 경로
-            status(chk);
-            System.out.println("최단경로는 "+count); //확인용 코드
-            shortest = Math.min(shortest, count); // 최단 거리 업데이트
-            return;
-        }
+            if(cur.x == n-1 && cur.y == m-1) return cur.dist; //도착
 
-        for(int i=0; i<4; i++) {
-            int nx = x + direc[i][0];
-            int ny = y + direc[i][1];
+            for(int i=0; i<4; i++) {
+                int nx = cur.x + direc[i][0];
+                int ny = cur.y + direc[i][1];
 
-            if(nx<0 || nx == n || ny<0 || ny == m) continue; //IndexOutOfArray
+                if (nx < 0 || nx == n || ny < 0 || ny == m) continue; //IndexOutOfArray
 
-            if (map[nx][ny] == 1) {
-                if(hit == 1) {
-                    System.out.println(ny + "," + nx + "의 벽을 부셨다."); //확인용 코드
-                    System.out.println(y+","+x+"에서 "+ny+","+nx+"로 이동합니다.");
-                    status(chk);
-                    dfs(nx, ny, 0, chk, count + 1); //벽 부수기
+                if (map[nx][ny] == 0) { //벽이 아님
+                    if (!chk[nx][ny][0] && !cur.hit) { //벽을 부신적이 없으면 chk[][][0] 방문
+                        q.add(new Point(nx, ny, cur.dist + 1, false));
+                        chk[nx][ny][0] = true;
+                    } else if (!chk[nx][ny][1] && cur.hit) { //벽을 부신적이 있으면 chk[][][1] 방문
+                        q.add(new Point(nx, ny, cur.dist + 1, true));
+                        chk[nx][ny][1] = true;
+                    }
+                } else { //벽임
+                    if (!cur.hit) {
+                        q.add(new Point(nx, ny, cur.dist + 1, true));
+                        chk[nx][ny][1] = true;
+                    }
                 }
             }
-            else {
-                System.out.println(y+","+x+"에서 "+ny+","+nx+"로 이동합니다.");
-                status(chk);
-                dfs(nx, ny, hit, chk, count + 1); // 이동
-            }
-
         }
-    }
-
-    public static void status(boolean[][] chk){
-        for(boolean[] i :chk){
-            for(boolean j : i) {
-                if(j)
-                    System.out.print("T ");
-                else
-                    System.out.print("F ");
-            }
-            System.out.println();
-        }
+        return -1;
     }
 }
