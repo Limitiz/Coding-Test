083e92d865a19d87fb3b64c19c728bb1be16c59a
Limitiz <60959811+Limitiz@users.noreply.github.com>
2022-03-04 15:05:29 +0900

    fix : 뒤에서부터 하면 경우의 수 줄일 수 있음
    StringBuilder sb에 대해서 sb.equlas(String) 안되고 sb.toString().equals(String) 해줘야함

--- BOJ/src/main/java/greedy/BOJ_12904.java
+++ BOJ/src/main/java/greedy/BOJ_12904.java
@@ -4,33 +4,41 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_12904 {
-
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
 		String t = br.readLine();
 		
-		Queue<String> q = new LinkedList();
-		q.add(s);
+		//거꾸로 t에서부터 s로 가기
+		Queue<StringBuilder> q = new LinkedList();
+		StringBuilder sb = new StringBuilder(t);
+		q.add(sb);
+		
 		while(!q.isEmpty()) {
-			String cur = q.poll();
-			//System.out.println(cur); //확인용 코드
+			StringBuilder cur = q.poll();
+			System.out.println(cur);
 			
-			if(cur.equals(t)) {
+			if(cur.equals(s)) {
 				System.out.println(1);
 				System.exit(0);
 			}
-			if(cur.length() == t.length()) continue;
-			q.add(cur+"A");
 			
-			char[] tmp = new char[cur.length()+1];
-			for(int i = 0; i<cur.length(); i++)
-				tmp[i] = cur.charAt(cur.length()-1 - i);
-			tmp[cur.length()] = 'B';
+			if(cur.length() == s.length()) continue;
 			
-			q.add(String.valueOf(tmp));
-			
+			StringBuilder nxt;
+      			//뒤에서 A 제거
+			if(cur.charAt(cur.length()-1) == 'A') {
+				nxt = new StringBuilder(cur.deleteCharAt(cur.length()-1));
+				q.offer(nxt);
+			}
+      			//B제거하고 뒤집기
+			else {
+				nxt = new StringBuilder(cur.deleteCharAt(cur.length()-1));
+				nxt.reverse();
+				q.offer(nxt);
+			}
 		}
+		
 		System.out.println(0);
 	}
 }
