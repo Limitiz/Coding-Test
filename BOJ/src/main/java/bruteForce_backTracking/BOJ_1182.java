8997ae759c62f320a5bc557a739fcd4be278965b
Limitiz <60959811+Limitiz@users.noreply.github.com>
2022-03-08 17:32:17 +0900

    create

---
+++ BOJ/src/main/java/bruteForce_backTracking/BOJ_1182.java
@@ -0,0 +1,36 @@
+package bruteForce_backTracking
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] seq;
+	static int n, s, cnt = 0;
+
+	public static void main(String[] args) throws IOException{
+		//입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		
+		seq = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) seq[i] = Integer.parseInt(st.nextToken());
+		
+		//완전탐색
+		dfs(0,0);
+		
+		if(s == 0) System.out.println(cnt-1);
+		else System.out.println(cnt);
+	}
+	
+	public static void dfs(int i, int sum) {
+		if(sum == s) cnt++; 
+		
+		if(i == n-1) return; 
+		
+		dfs(i+1, sum+seq[i]); //더하고 넘어가기
+		dfs(i+1, sum); //안 더하고 넘어가기
+	}
+}
