5af01ffbd433af04e18d1b26b9560899beb7f502
Limitiz <ll55kk678@gmail.com>
2022-03-14 23:12:43 +0900

    fix:시간초과라 dp로 품

--- BOJ/src/main/java/bruteForce_backTracking/BOJ_11722.java
+++ BOJ/src/main/java/bruteForce_backTracking/BOJ_11722.java
@@ -6,25 +6,19 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class BOJ_11722 {
-    static int[] a;
-    static int n, max = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(br.readLine());
+        int a[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int dp[] = new int[n];
+        int max = 0, num = 0;
 
-        dfs(0,0);
-        System.out.println(max);
-    }
-
-    public static void dfs(int i, int len){
-        if(len == n-1) {
-            max = Math.max(max, len);
-            return;
+        //dp
+        dp[0] = 1; num = a[0];
+        for(int i=1; i<n; i++){
+            if(a[i]<a[i-1]) dp[i]++;
         }
 
-        if(a[i+1]<a[i]) dfs(i+1, len+1); //감소하는 수열 추가
-        dfs(i+1, len); //추가 안하고 넘어가기
+        System.out.println(max);
     }
 }
