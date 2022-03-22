9b626d48d5005cc1e8636f743f46ef89a5222aac
Lim-Jiyoung <60959811+Limitiz@users.noreply.github.com>
2022-03-22 16:01:24 +0900

    fix : 메모이제이션 활용
    ㅋㅋㅋ 그 전 코드 O(2^n)의 쓰레기 코드였다.
    DP는 메모이제이션을 사용해야지

--- BOJ/src/main/java/dynamicP/BOJ_2670.java
+++ BOJ/src/main/java/dynamicP/BOJ_2670.java
@@ -3,29 +3,21 @@ package dynamicP;
 import java.io.*;
 
 public class BOJ_2670 {
-	static double[] sequence;
-	static double max = 0;
-	static int n;
-	
 	public static void main(String[] args) throws IOException{
 		//input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+        double max = 0;
 		
-		sequence = new double[n];
+		double[] sequence = new double[n];
 		for(int i=0; i<n; i++) sequence[i] = Double.parseDouble(br.readLine());
 
 		//dp
-		dp(0,1);
+		for(int i = 1; i < n; i++) {
+			sequence[i] = Math.max(sequence[i], sequence[i-1] * sequence[i]);
+			max = Math.max(max, sequence[i]);
+		}
 		System.out.printf("%.3f", max);
 		
-	}
-	
-	public static void dp(int i, double result) {
-		if(i == n) return;
-		
-		max = Math.max(max, result);
-		dp(i+1, 1); //안곱하고 넘어감
-		dp(i+1, result*sequence[i]); //곱하고 넘어감
 	}
 }
