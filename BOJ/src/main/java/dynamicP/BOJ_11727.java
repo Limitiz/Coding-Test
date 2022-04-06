65df18fb396624518d87f7a975ae4acff567c44e
Lim-Jiyoung <60959811+Limitiz@users.noreply.github.com>
2022-04-06 23:30:39 +0900

    fix : [11727번 2xn 타일링2]
     if문 없으면 n==1일때 ArrayIndexOutofBound 에러남

--- BOJ/src/main/java/dynamicP/BOJ_11727.java
+++ BOJ/src/main/java/dynamicP/BOJ_11727.java
@@ -9,6 +9,12 @@ public class BOJ_11727 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n+1];
+        
+        if(n == 1){
+            System.out.println(1);
+            System.exit(0);
+        }
+        
         dp[1] = 1; dp[2] = 3;
         for(int i=3; i<=n; i++)
             dp[i] = (2*dp[i-2] + dp[i-1])%10007;
